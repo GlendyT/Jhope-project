44a1cf735017478df75475646c3ee483ce0e8391
GlendyT <glendytuyuc@gmail.com>
2024-02-03 19:51:23 -0600

    apply jaimes changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,18 +4,18 @@ import jitb1 from "../assets/jitb1.png";
 import jitbhe1 from "../assets/jitbHE1.png";
 import card4 from "../assets/1.png"
 //IMPORTS PARA DESCARGAR IMAGEN
-import { useRef } from "react";
+import { useCallback, useRef, useEffect, useState  } from "react";
 import { toPng } from "html-to-image";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import TicketComponent from "./ticketComponent/TicketComponent";
+
 
 const diccionarioIconos = {
   HopeWorld: HP1,
   JackInTheBox: jitb1,
   JackInTheBoxHOPEEdition: jitbhe1,
 };
-
-export function Home({ user, setUser, albu, song }) {
 
   const notify = () => {
     toast.info('🐿️Downloading Your Ticket!', {
@@ -30,64 +30,61 @@ export function Home({ user, setUser, albu, song }) {
       });
   }; 
 
+  export function Home({ user, setUser, albu, song }) {
+
+    const [imageSaved, setImageSaved] = useState(false);
+
+    useEffect(() => {
+        if(imageSaved) {
+          const timer = setTimeout(() => {
+            notify();
+            setImageSaved(false);
+          }, 3000);
+  
+          return () => clearTimeout(timer);
+        }
+    }, [imageSaved]);
+  
+
   //Para descargar la imagen
   const elementRef = useRef(null);
-  const htmlToImageConvert = () => {
+  const htmlToImageConvert = useCallback(() => {
+    if(!elementRef.current) return;
+
     toPng(elementRef.current, { cacheBust: false })
       .then((dataUrl) => {
         const link = document.createElement("a");
         link.download = "Hobipalooza";
         link.href = dataUrl;
+
+        link.addEventListener('click', () => {
+          setImageSaved(true); // Set the state to true when the download starts
+        });
+
         link.click();
+        
       })
       .catch((err) => {
         console.log(err);
       });
-  };
+  }, [elementRef]);
 
   const handleLogout = () => {
     setUser({});
   };
   return (
     <>
-      <div className="titulo-card">HAPPY HOBIUARY</div>
-      <div className="titulo">
-        <h1>Here is your ticket to keep loving and supporting j-hope</h1>
-        <h2>Scan the QR code to find out a Surprise</h2>
-      </div>
-        <div  className="contenedor-ticket" ref={elementRef} >
-          <div className="ticket-inside">
-            <div className="titulo-user">
-              Name: <div className="titulo-user1">{user}</div>
-            </div>
-            <div className="titulo-user">
-              City: <div className="titulo-user1">{albu}</div>
-            </div>
-            <div className="titulo-user">
-              Location: <div className="titulo-user1">{song}</div>
-            </div>
-          </div>
-          <img
-            src={diccionarioIconos[albu]}
-            typeof="image/png"
-            alt="Card"
-            className="pic3"
-         />
-        </div>
-
-        <div className="boton-container"> 
-      <div className="boton-container2">
-      <div className="boton2">
-        <button onClick={() => {
-          htmlToImageConvert();
-          notify();
-        }}>Download</button>
-        <ToastContainer/>
-      </div>
-      <div className="boton3">
-        <button onClick={handleLogout}>Log Out</button>
-      </div>
-      </div>
+      <div>
+        <TicketComponent
+          user={user}
+          albu={albu}
+          song={song}
+          elementRef={elementRef}
+          handleLogout={handleLogout}
+          htmlToImageConvert={htmlToImageConvert}
+          notify={notify}
+        />
+        <ToastContainer />
       </div>
     </>
   );
